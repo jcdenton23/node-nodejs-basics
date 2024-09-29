bc0a5d3796dbee5c939581141503e651e9be23ef
Ali <alisher_tashbulatov@epam.com>
2024-09-29 23:05:21 +0500

    feat: implement streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(stdout);
 };
 
 await read();
