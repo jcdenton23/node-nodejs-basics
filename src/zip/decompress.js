d9cf782b31db659ec995544e53305cde0fd678cb
Ali <alisher_tashbulatov@epam.com>
2024-09-29 23:22:38 +0500

    feat: implement zip task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourceFilePath = path.join(__dirname, 'files', 'archive.gz');
+const destFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  fs.createReadStream(sourceFilePath).pipe(zlib.createGunzip()).pipe(fs.createWriteStream(destFilePath));
 };
 
 await decompress();
