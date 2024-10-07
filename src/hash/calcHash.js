fbcec33f90916d79a1ef086ce999ec5c8627bc35
Ali <alisher_tashbulatov@epam.com>
2024-10-07 20:24:13 +0500

    fix: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,17 +1,36 @@
 import { createHash } from 'crypto';
 import fs from 'fs';
 import path from 'path';
-import { stdout } from 'process';
 import { fileURLToPath } from 'url';
 
-const hash = createHash('sha256');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 
+const calculateHashHelper = () => {
+  return new Promise((resolve, reject) => {
+    const hash = createHash('sha256');
+    const input = fs.createReadStream(filePath);
+
+    input.on('error', (err) => {
+      reject(`Error reading file: ${err.message}`);
+    });
+
+    hash.on('finish', () => {
+      resolve(hash.digest('hex'));
+    });
+
+    input.pipe(hash);
+  });
+};
+
 const calculateHash = async () => {
-  const input = fs.createReadStream(filePath);
-  input.pipe(hash).setEncoding('hex').pipe(stdout);
+  try {
+    const hashValue = await calculateHashHelper();
+    console.log(hashValue);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await calculateHash();
