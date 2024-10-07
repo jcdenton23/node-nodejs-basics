3463e57ae6ede2fbdcd04a398b2ffeebca27188c
Ali <alisher_tashbulatov@epam.com>
2024-10-07 20:01:14 +0500

    fix: args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,16 @@
 const parseArgs = () => {
+  const result = [];
+
   process.argv.forEach((arg, index, args) => {
     if (arg.startsWith('--')) {
       const value = args[index + 1];
       if (value && !value.startsWith('--')) {
-        console.log(`${arg.slice(2)} is ${value}`);
+        result.push(`${arg.slice(2)} is ${value}`);
       }
     }
   });
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
